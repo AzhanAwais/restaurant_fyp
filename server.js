d16e53e6cbbe19c6d7efe0ef3a4759b2976a8dfb
shaheer924 <39200747+shaheer924@users.noreply.github.com>
2023-10-29 15:18:19 +0500

    Error Middleware added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const authRoute = require("./routes/authRoute")
 const userRoute = require("./routes/userRoute")
 const authController = require("./controllers/authController")
+const ErrorMiddleware = require("./middlewares/ErrorMiddleware")
 
 
 // use middlewares
@@ -13,13 +14,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-
-
 // use routes
 app.use(authRoute)
 app.use(userRoute)
-
-
+app.use(ErrorMiddleware)
 
 app.listen(PORT, () => {
     console.log(`listen port ${PORT}`)
